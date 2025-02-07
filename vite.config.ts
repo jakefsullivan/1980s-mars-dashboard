8d933ae3bfe380c520d740ef4f590ffeb307f87e
jake-sullivan-spin <jakefsullivan@gmail.com>
2025-02-07 10:04:53 -0600

    code cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true, // Use global variables like 'describe', 'it', etc.
-    environment: 'jsdom', // Provides a DOM environment for React components
-    setupFiles: './src/test/setup.ts', // Optional: setup file to configure testing utilities
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
   },
 });
